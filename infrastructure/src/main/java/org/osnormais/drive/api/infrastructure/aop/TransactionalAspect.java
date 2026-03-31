c33b2a5fc2818dca4cf3098d2ae94368636764e1
jhonatapers <jhonatapers@gmail.com>
2026-03-31 18:58:03 -0300

    refactor: substitui gestão customizada de transações por TransactionalSupplier

--- infrastructure/src/main/java/org/osnormais/drive/api/infrastructure/aop/TransactionalAspect.java
+++ infrastructure/src/main/java/org/osnormais/drive/api/infrastructure/aop/TransactionalAspect.java
@@ -5,10 +5,10 @@ import static java.util.Objects.requireNonNull;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.osnormais.drive.api.infrastructure.commons.ExceptionWrapper;
+import org.osnormais.drive.api.infrastructure.commons.transaction.TransactionalSupplier;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 @Aspect
@@ -25,39 +25,16 @@ public class TransactionalAspect {
     @Around("@annotation(org.osnormais.drive.api.application.common.annotation.Transactional)")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
-            return transactionTemplate.execute(new Transaction(joinPoint));
-        } catch (final TransactionalAspect.Transaction.TransactionException e) {
+            return transactionTemplate.execute(TransactionalSupplier.of(() -> {
+                try {
+                    return joinPoint.proceed();
+                } catch (final Throwable e) {
+                    throw ExceptionWrapper.wrap(e);
+                }
+            }));
+        } catch (final ExceptionWrapper e) {
             throw e.getCause();
         }
-    }
-
-    class Transaction implements TransactionCallback<Object> {
-
-        private final ProceedingJoinPoint joinPoint;
-
-        Transaction(final ProceedingJoinPoint joinPoint) {
-            this.joinPoint = requireNonNull(joinPoint);
-        }
-
-        @Override
-        public Object doInTransaction(TransactionStatus status) {
-            try {
-                final var result = joinPoint.proceed();
-                return result;
-            } catch (Throwable e) {
-                status.setRollbackOnly();
-                throw new TransactionException(e);
-            }
-        }
-
-        static class TransactionException extends RuntimeException {
-
-            TransactionException(Throwable cause) {
-                super(cause);
-            }
-
-        }
-
     }
 
 }
