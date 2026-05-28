f42c3e9a5cd4d01753e3bc2bf35ff249b5fd559a
jhonatapers <jhonatapers@gmail.com>
2026-05-28 14:28:24 -0300

    feat: update security configuration to require authentication for all requests

--- infrastructure/src/main/java/org/osnormais/drive/api/infrastructure/configuration/security/SecurityConfig.java
+++ infrastructure/src/main/java/org/osnormais/drive/api/infrastructure/configuration/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                                 "/swagger-ui.html")
                         .permitAll()
                         .anyRequest()
-                        .permitAll())
+                        .authenticated())
                 .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakJwtConverter())))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
